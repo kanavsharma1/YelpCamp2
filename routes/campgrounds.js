3f699bbc697b2bda1cbfb09c9d177c4a3c7d085c
kanav sharma <kanavsharma1@outlook.com>
2019-08-02 15:43:33 +0530

    Editing and updating campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -46,6 +46,26 @@ router.get("/:id", (req, res) => {
     });
 });
 
+//EDIT ROUTE
+router.get("/:id/edit", (req, res) => {
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        if (err) res.redirect("/campgrounds");
+        else res.render("campgrounds/edit", { campground: foundCampground });
+    });
+
+});
+
+//UPDATE ROUTE
+router.put("/:id", (req, res) => {
+    //find and update correct campground
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
+        console.log("under find and update query")
+        if (err) res.redirect("/campgrounds");
+        else res.redirect("/campgrounds/" + req.params.id)
+    })
+    //redirect
+});
+
 //middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
